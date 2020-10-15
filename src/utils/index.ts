36d9493ea48b637df84f04afb3530b3be5741a92
GabrielModog <gabrielmodog@gmail.com>
2020-10-15 16:46:03 -0300

    add feature to convert units

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export const poundsToKilo = (value: number) => {
+  return Number(value * 2.2046).toFixed(2);
+};
+
+export const feetToMeters = (value: number) => {
+  return Number(value / 3.2808).toFixed(2);
+};

b829359fba6a7cdd90f3c1b891d62207c65052e9
GabrielModog <gabrielmodog@gmail.com>
2020-10-15 16:47:58 -0300

    add pokemon page

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { searchRequest } from '../../shares/ducks/pokemons/actions';
+import {
+  Container,
+  Information,
+  Loading,
+  Button,
+  PokeData,
+  Row,
+  TypesCard,
+  TypesItemCard,
+  TypesItemContainer,
+} from './styles';
+import { feetToMeters, poundsToKilo } from '../../utils';
 
 const Pokemon = () => {
   const { pokeid } = useParams<any>();
+  const dispatch = useDispatch();
+  const pokemon = useSelector((state: any) => state.pokemons.data);
 
-  return <div>{pokeid}</div>;
+  useEffect(() => {
+    dispatch(searchRequest(pokeid));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(pokemon);
+
+  return (
+    <Container>
+      <Information>
+        <div>
+          <Button type="button">VOLTAR</Button>
+        </div>
+        <PokeData>
+          {pokemon.loading ? <Loading /> : ''}
+          {pokemon.error ? 'Ocorreu um erro...' : ''}
+          {pokemon && (
+            <>
+              <h1>{pokemon[0]?.name.toUpperCase()}</h1>
+              <div>
+                <img src={pokemon[0]?.sprites?.front_default} alt="" />
+              </div>
+              <div>
+                <Row>
+                  <div>
+                    <h1>{pokemon[0]?.weight} libras</h1>
+                    <h1>{pokemon[0]?.height} pés</h1>
+                  </div>
+                  <div>
+                    <h1>{poundsToKilo(pokemon[0]?.weight)} kg</h1>
+                    <h1>{feetToMeters(pokemon[0]?.height)} metros</h1>
+                  </div>
+                </Row>
+
+                <TypesCard>
+                  <h3>Tipo(s) do Pokémon</h3>
+                  <TypesItemContainer>
+                    {pokemon[0]?.types &&
+                      pokemon[0]?.types.map((item: any) => (
+                        <TypesItemCard key={item.type.name}>
+                          {item.type.name}
+                        </TypesItemCard>
+                      ))}
+                  </TypesItemContainer>
+                </TypesCard>
+              </div>
+            </>
+          )}
+        </PokeData>
+      </Information>
+    </Container>
+  );
 };
 
 export default Pokemon;
---
+++ src/pages/Pokemon/styles.ts
@@ -0,0 +1,97 @@
+import styled from 'styled-components';
+import { Danger, Warning, White } from '../../styles/palette';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 3rem 3rem 3rem;
+  height: 100vh;
+`;
+
+export const Information = styled.div`
+  width: 960px;
+  // height: 306px;
+  background: #ffffff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`;
+
+export const PokeData = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+`;
+
+export const TypesCard = styled.div`
+  background-color: ${Warning[200]};
+  width: 400px;
+  padding: 2rem;
+  color: white;
+  border-radius: 10px;
+  margin: 2rem 0;
+
+  h3 {
+    font-weight: 400;
+  }
+`;
+
+export const TypesItemContainer = styled.div`
+  display: flex;
+`;
+
+export const TypesItemCard = styled.div`
+  padding: 5px;
+  background-color: ${Warning[400]};
+  margin: 2px;
+  border-radius: 5px;
+  font-size: 12px;
+`;
+
+export const Loading = styled.div`
+  border-radius: 30rem;
+  border: 5px dashed ${Danger[0]};
+  width: 34px;
+  height: 34px;
+  animation: spinner 500ms linear infinite;
+
+  @keyframes spinner {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export const Button = styled.button`
+  padding: 0 2rem;
+  height: 42px;
+  background: ${Danger[0]};
+  border-radius: 10px;
+  border: none;
+  color: ${White[0]};
+  font-weight: 500;
+  transition: 450ms;
+  outline: none;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.9;
+  }
+
+  &:active {
+    opacity: 0.8;
+  }
+`;

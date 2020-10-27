ea09e811707dd84602ab4791c1f2591ce1488413
GabrielModog <gabrielmodog@gmail.com>
2020-10-26 22:29:31 -0300

    remove unused import

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { loadRequest } from '../../shares/ducks/pokemons/actions';
 import { Button, Container } from './styles';
